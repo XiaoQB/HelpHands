2c0a4b928fb8922aacebf03d0f9cade0d785fd6c
fuwuchen <odtuntun@gmail.com>
2022-05-19 13:19:26 +0800

    init project with mvn

---
+++ helphands-api/src/main/java/org/example/hello/api/GreetingMessage.java
@@ -0,0 +1,19 @@
+package org.example.hello.api;
+
+import lombok.Value;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.base.Preconditions;
+
+@Value
+@JsonDeserialize
+public final class GreetingMessage {
+
+  public final String message;
+
+  @JsonCreator
+  public GreetingMessage(String message) {
+    this.message = Preconditions.checkNotNull(message, "message");
+  }
+}
