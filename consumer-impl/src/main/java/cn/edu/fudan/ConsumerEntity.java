32d878f8529349f9c85a4c5920468809866c396c
QuanbinXiao <plus2019@foxmail.com>
2022-07-19 10:41:55 +0800

    feat: add restartWithBackoff supervisor strategy for Order and Consumer module

--- consumer-impl/src/main/java/cn/edu/fudan/ConsumerEntity.java
+++ consumer-impl/src/main/java/cn/edu/fudan/ConsumerEntity.java
@@ -1,5 +1,6 @@
 package cn.edu.fudan;
 
+import akka.actor.typed.SupervisorStrategy;
 import akka.cluster.sharding.typed.javadsl.EntityContext;
 import akka.cluster.sharding.typed.javadsl.EntityTypeKey;
 import akka.persistence.typed.PersistenceId;
@@ -7,6 +8,7 @@ import akka.persistence.typed.javadsl.*;
 import cn.edu.fudan.domain.consumer.ConsumerDTO;
 import cn.edu.fudan.domain.consumer.ConsumerParam;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Objects;
 
@@ -30,7 +32,8 @@ public class ConsumerEntity
                 PersistenceId.of(
                         entityContext.getEntityTypeKey().name(),
                         entityContext.getEntityId()
-                )
+                ),
+                SupervisorStrategy.restartWithBackoff(Duration.ofSeconds(2), Duration.ofSeconds(30), 0.2)
         );
         this.entityContext = entityContext;
         this.entityId = entityContext.getEntityId();
